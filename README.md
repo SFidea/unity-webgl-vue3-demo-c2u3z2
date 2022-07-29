6667276791dd21cf92ac0cf2dbeffb561d9959fe
matseriksen <mats.holmeriksen@gmail.com>
2022-07-29 21:51:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# unity-webgl-vue3-demo-c2u3z2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/unity-webgl-vue3-demo-c2u3z2)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import UnityWebgl from 'unity-webgl';
+
+const canvasRef = ref(null);
+let unityComponent = null;
+
+onMounted(() => {
+  if (!unityComponent) {
+    unityComponent = new UnityWebgl(canvasRef.value, {
+      loaderUrl:
+        'https://static-huariot-com.oss-cn-hangzhou.aliyuncs.com/unity/test1/Build/OUT_BIM.loader.js',
+      dataUrl:
+        'https://static-huariot-com.oss-cn-hangzhou.aliyuncs.com/unity/test1/Build/OUT_BIM.data',
+      frameworkUrl:
+        'https://static-huariot-com.oss-cn-hangzhou.aliyuncs.com/unity/test1/Build/OUT_BIM.framework.js',
+      codeUrl:
+        'https://static-huariot-com.oss-cn-hangzhou.aliyuncs.com/unity/test1/Build/OUT_BIM.wasm',
+    });
+  }
+});
+onBeforeUnmount(() => {
+  if (unityComponent) {
+    unityComponent.destroy();
+    unityComponent = null;
+  }
+});
+</script>
+
+<template>
+  <div style="width: 800px; height: 600px; border: 1px solid #f00">
+    <canvas id="canvas" ref="canvasRef" style="width: 100%; height: 100%" />
+  </div>
+</template>
